be520ca0e78b60837cdcc9058d0e749696c6b3ab
ritwikbd <2021mt93141@wilp.bits-pilani.ac.in>
2023-04-09 17:30:33 +0530

    Productionisation done

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'explai',         # How you named your package folder (MyLib)
   packages = ['explai'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A reporting tool for explainable AI',   # Give a short description about your library
   author = 'Ritwik Bandyopadhyay',                   # Type in your name
   author_email = 'ritwikbanrg@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ritwikbd/explai',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ritwikbd/explai/archive/refs/tags/v01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ritwikbd/explai/archive/refs/tags/v03.tar.gz',    # I explain this later on
   keywords = ['Explainable AI', 'Machine Learning'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'reportlab','scikit-learn','matplotlib','seaborn','pandas'
