defd6e8a0d7fa775f5cbbf93cde556598f8682c9
ritwikbd <2021mt93141@wilp.bits-pilani.ac.in>
2023-04-09 11:47:46 +0530

    Release version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Ritwik Bandyopadhyay',                   # Type in your name
   author_email = 'ritwikbanrg@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ritwikbd/explai',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ritwikbd/explai/archive/refs/tags/v01.tar.gz',    # I explain this later on
   keywords = ['Explainable AI', 'Machine Learning'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'reportlab','sklearn','matplotlib','seaborn','pandas'
